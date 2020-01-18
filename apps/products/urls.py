989c82f0f7e488a148fc2b444f9f88d0644ce90c
273620484 <51697459+linmaozhang@users.noreply.github.com>
2020-01-18 11:53:49 +0800

    规格与类别

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,5 +3,7 @@ from django.urls import re_path
 
 from apps.products import views as product
 urlpatterns = [  
-    re_path('^specifications/$',product.SpecificationsView.as_view())
+    re_path('^specifications/$',product.SpecificationsView.as_view()),
+    re_path('^specifications_values/$',product.SpecificationValuesView.as_view()),
+    re_path('^product_type/$',product.MerchantProductTypeView.as_view())
 ]
